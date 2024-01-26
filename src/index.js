213e7404e8233a1bc89ebf47e546073e1eb86a35
lukeatki <luke24aal@gmail.com>
2024-01-25 22:46:03 -0500

    service complete

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// if the origin is not the same, accept it, but we can specify
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - Hello World!');
+});
+
+app.get('/add/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) + Number(req.params.m));
+});
+
+app.listen(port);
